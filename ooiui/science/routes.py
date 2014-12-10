b95336960d760e228241374fbb09a53b7e7561ee
Luke Campbell <luke.s.campbell@gmail.com>
2014-12-10 11:32:57 -0500

    Adds endpoint to allow ERDDAP to be configured for staging

--- ooiui/science/routes.py
+++ ooiui/science/routes.py
@@ -6,7 +6,7 @@ Defines the application routes
 '''
 from ooiui.science.app import app
 from flask import request, render_template, Response
-from ooiui.config import TABLEDAP, SERVICES_URL, DEBUG, ERDDAP_URL
+from ooiui.config import TABLEDAP, SERVICES_URL, DEBUG, ERDDAP_URL, SERVICES_CONFIG
 
 from ooiui.science.interface import tabledap as tabled
 from ooiui.science.interface.toc import get_toc as get_toc_interface, flush_cache
@@ -19,6 +19,7 @@ from datetime import datetime,timedelta
 import time
 import numpy as np
 import math
+import yaml
 
 @app.route('/pioneer/')
 def pioneer():
@@ -70,4 +71,27 @@ def flush():
 @app.route('/')
 def root():
     return render_template('index.html', erddap_url=ERDDAP_URL)
+
+@app.route('/update_erddap/<path:dir_path>')
+def update_erddap(dir_path):
+    dir_path = '/' + dir_path
+    response = {}
+    if not os.path.exists(dir_path):
+        response['status'] = 'fail'
+        response['message'] = 'Directory %s does not exist' % dir_path
+        response = Response(response=json.dumps(response), status=500, mimetype='application/json')
+        return response
+    dir_path = dir_path.encode('ascii', 'ignore')
+
+    with open(SERVICES_CONFIG, 'r') as f:
+        doc = yaml.load(f.read()) or {}
+        doc['DATASETS_DIR'] = dir_path
+    with open(SERVICES_CONFIG, 'w') as f:
+        f.write(yaml.dump(doc, default_flow_style=False))
+
+
+    response['status'] = 'ok'
+    response['path'] = dir_path
+    response = Response(response=json.dumps(response), status=200, mimetype='application/json')
+    return response
 
